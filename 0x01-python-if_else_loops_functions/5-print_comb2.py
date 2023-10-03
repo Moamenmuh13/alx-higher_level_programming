96d0b03eed781dd7486ffc91eef1e0867a9ddc46
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-03 18:11:19 +0300

    print a num of 2 digit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number == 99:
-        print(f"{number}",'\n', end='')
+        print('{:02d}'.format(number),'\n', end='')
     else:
-        print(f"{number:02d}, ", end='')
+        print('{:02d}, '.format(number), end='')
