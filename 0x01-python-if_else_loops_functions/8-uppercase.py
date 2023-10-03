d51fe22bcaa3118e0683c601d8a3690b91147c7d
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-03 18:58:42 +0300

    covert small char to big

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = ""
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
-         result += (chr(ord(char) - 32))
-        else:
-         result += char
-    print("{}".format(result))
+            print(chr(ord(char) - 32), end='')
+    print()
+    
