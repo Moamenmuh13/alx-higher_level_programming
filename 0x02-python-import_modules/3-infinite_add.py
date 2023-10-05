888e093c6935c0880d3abe4ed270482d497dadb1
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-05 20:25:22 +0300

    Finishing man tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """ "Print the additin of all arguments"""
     import sys
 
     total = 0
     for i in sys.argv[1:]:
         total = total + int(i)
-        
     print("{:d}".format(total))
