dbe80ea129f2140da9e0adfd056017e4282b6d01
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-11 23:30:43 +0300

    pycode style for 12 tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 
 
 def roman_to_int(roman_string):
-    roman_values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_values = {"I": 1, "V": 5, "X": 10, "L": 50,
+                    "C": 100, "D": 500, "M": 1000}
 
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
@@ -12,9 +13,11 @@ def roman_to_int(roman_string):
     while i < len(roman_string):
         if (
             i + 1 < len(roman_string)
-            and roman_values[roman_string[i]] < roman_values[roman_string[i + 1]]
+            and roman_values[roman_string[i]] < 
+                roman_values[roman_string[i + 1]]
         ):
-            total += roman_values[roman_string[i + 1]] - roman_values[roman_string[i]]
+            total += roman_values[roman_string[i + 1]] 
+            - roman_values[roman_string[i]]
             i += 2
         else:
             total += roman_values[roman_string[i]]
