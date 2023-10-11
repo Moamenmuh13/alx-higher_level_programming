6f74bcd9d20bef493fc9c4653122ad034c283c3a
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-11 23:28:28 +0300

    finishing remaining tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary:
+        best_score = max(a_dictionary, key=a_dictionary.get)
+        return best_score
+    return None
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    roman_values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    total = 0
+    i = 0
+    while i < len(roman_string):
+        if (
+            i + 1 < len(roman_string)
+            and roman_values[roman_string[i]] < roman_values[roman_string[i + 1]]
+        ):
+            total += roman_values[roman_string[i + 1]] - roman_values[roman_string[i]]
+            i += 2
+        else:
+            total += roman_values[roman_string[i]]
+            i += 1
+    return total
