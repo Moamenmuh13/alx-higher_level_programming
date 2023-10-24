9a52bcf9385833049b0a115a7acf525568aca643
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-24 22:06:41 +0300

    return area for the sqaure

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """
+    This class defines a square by the size attribute.
+    """
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int) : The size of the sqaure."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
