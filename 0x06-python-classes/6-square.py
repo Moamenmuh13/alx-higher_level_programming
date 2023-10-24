be7c6c3ffb7a40e0597846cb7bb20681e1502318
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-24 23:05:18 +0300

    fix task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,11 +49,12 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
 
-        if self.size == 0:
-            print()
-        else:
-            for i in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                print("_" * self.__position[0] + "#" * self.__size + "$")
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
