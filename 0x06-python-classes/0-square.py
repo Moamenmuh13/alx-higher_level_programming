ae6c5eb815377b61f741818424132ae3769c8104
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-24 21:46:11 +0300

    initail commit and done task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Define a class square"""
+
+
+class Square:
+    """
+    This is an empty Square class.
+    It does not have any attributes or methods at the moment.
+    """
+
+    pass
