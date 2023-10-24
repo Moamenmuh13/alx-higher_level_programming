f218feccd5f8b4ebed3c4451210c1663189110c4
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-24 21:49:55 +0300

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Define a class square"""
+
+
+class Sqaure:
+    """
+    This class defines a square by the size attribute.
+    """
+
+    def __init__(self, size):
+        self.__size = size
