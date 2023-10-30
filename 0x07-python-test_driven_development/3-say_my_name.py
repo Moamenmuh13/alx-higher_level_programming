cc6550888153fbde59fcaad203feeed05efc28b6
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-10-31 01:48:51 +0200

    Finish all task

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
+"""
+    Print full name.
+
+    Raises:
+          TypeError: first_name must be a string or 
+                    last_name must be a string
+
+    Args:
+        first_name (str): the first name
+        last_name (str): the second name. default value empty string
+
+    Returns:
+        str : Prints the full name
+
+    """
 
 
 def say_my_name(first_name, last_name=""):
-    pass
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    return "My name is {} {}$".format(str(first_name), str(last_name))
