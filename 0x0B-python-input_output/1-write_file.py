fa4e19248d2446f46c45f18ded12c050a73cddc4
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-11-08 00:36:06 +0200

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" Define a write a text in file function"""
+
+
 def write_file(filename="", text=""):
+    """Return the length of the text"""
     with open(filename, "w", encoding="utf-8") as file:
-        pass
+        character = file.write(text)
+
+    return character
