9dd6cd2daed5e62009e5b813ad7ca4063a7f5ea4
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-11-08 01:03:27 +0200

    finish all tasks

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
         my_obj (Any): The Python object to be serialized to JSON.
         filename (str): The name of the file to save the JSON representation to.
     """
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "w") as file:
         json.dump(my_obj, file)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Defines a JSON file-writing function."""
 
 import json
 
 
 def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+
     with open(filename) as file:
         return json.load(file)
