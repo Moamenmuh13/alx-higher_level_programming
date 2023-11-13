febf934b462be050d03ba77fb15a700da5195254
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2023-11-13 18:46:22 +0200

    fixing all tasks without unit testing

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,13 +31,17 @@ class Base:
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file."""
         filename = cls.__name__ + ".json"
-        json_str = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        json_str = cls.to_json_string(
+            [obj.to_dictionary() for obj in list_objs]
+            )
         with open(filename, "w") as file:
             file.write(json_str)
 
     @staticmethod
     def from_json_string(json_string):
-        """Returns the list of dictionaries from the JSON string representation."""
+        """Returns the list of dictionaries
+        from the JSON string representation.
+        """
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
